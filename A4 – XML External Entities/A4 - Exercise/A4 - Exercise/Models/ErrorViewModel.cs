bf8c64512ed6497733d2e875903d680267c4c387
Don Kenneth <vitugdonkenneth@gmail.com>
2020-02-12 05:39:51 +0800

    updates

---
+++ A4 – XML External Entities/A4 - Exercise/A4 - Exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace A4___Exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
